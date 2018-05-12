57e8d5aaa69f63998ea6300e63344af5fd53a43c
jbaldwin <joshrbaldwin@gmail.com>
2018-05-12 15:19:11 -0600

    Simple build working

---
+++ inc/vidar/ConnectionInfo.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace vidar
+{
+
+auto hello_world() -> void;
+
+} // namespace vidar
---
+++ src/ConnectionInfo.cpp
@@ -0,0 +1,13 @@
+#include "vidar/ConnectionInfo.h"
+
+#include <iostream>
+
+namespace vidar
+{
+
+auto hello_world() -> void
+{
+    std::cout << "Hello World!" << std::endl;
+}
+
+} // namespace vidar
